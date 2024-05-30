27decb97c7c530858f94af79632da235e5be3d6e
JesusSaucedo22 <jesusemmanuelsaucedocardenas@gmail.com>
2024-05-30 12:04:30 -0600

    Commit´s

---
+++ 7 IndiceDeMasa/IndiceDeMasaCorporal/IndiceDeMasaCorporal.CalculadorImc/CalculadoraDeIndiceCorporal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace IndiceDeMasaCorporal.CalculadoraImc
+{
+    public static class CalculadoraDeIndiceDeMasaCorporal
+    {
+        public static decimal IndiceDeMasaCorporal(decimal peso, decimal estatura)
+        {
+            return peso / (estatura * estatura);
+        }
+        public static string EstadoNutricional(decimal imc)
+        {
+            if (imc < 18.5M)
+            {
+                return "peso bajo";
+            }
+            else if (imc < 25.0M)
+            {
+                return "Peso normal";
+            }
+            else if (imc < 30.0M)
+            {
+                return "Sobrepeso";
+            }
+            else if (imc < 40.0M)
+            {
+                return "Obesidad";
+            }
+            else
+            {
+                return "Obesidad extrema";
+            }
+
+        }
+    }
+}
+
---
+++ 7 IndiceDeMasa/IndiceDeMasaCorporal/IndiceDeMasaCorporal.ConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using IndiceDeMasaCorporal.CalculadoraImc;
+Console.WriteLine("Aplicacion que calcula el indice de masa corporal de una persona\n");
+
+try
+{
+    decimal peso = ReadDecimal("Proporcione el peso en kilogramos de la persona");
+    decimal estatura = ReadDecimal("Proporcione la estatura en metros de la persona");
+
+    decimal imc = CalculadoraDeIndiceDeMasaCorporal.IndiceDeMasaCorporal(peso, estatura);
+    Console.WriteLine($"El indice de masa corporal de la persona es {imc:F4}");
+    Console.WriteLine($"El estado nutricional de la persona es {CalculadoraDeIndiceDeMasaCorporal.EstadoNutricional(imc)}");
+}
+catch (FormatException)
+{
+    Console.Error.WriteLine("El numero proporcionadp no tiene el formato correcto.");
+    //throw;
+}
+catch (OverflowException)
+{
+    Console.Error.WriteLine("El numero proporcionado es muy grande.");
+    //throw;
+}
+catch (Exception exc)
+{
+    Console.Error.WriteLine(exc.Message);
+    Console.Error.WriteLine(exc.StackTrace);
+}
+decimal ReadDecimal(string s)
+{
+    //Decimal value;
+    //bool huboExito = false;
+    Console.WriteLine(s);
+    string entrada = Console.ReadLine();
+    //huboexito = decimal TryParse(entrada, out value);
+    //if (huboExito)
+    //{
+    //    return value;
+    //}
+    //else
+    //{
+    //throw new Exception("El valor proporcionado no es un numero");
+    //}
+    return Convert.ToDecimal(entrada);
+}
+
+
